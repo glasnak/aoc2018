a09eacf4a89a3e464d8c74d348497a2d9a232e0d
Ondrej Glasnak <glasnak.o@gmail.com>
2018-12-07 09:56:04 +0100

    Separate Coord to be usable by Day06 and presumably others

---
+++ Coord.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+struct Coord {
+  int x;
+  int y;
+
+  Coord() : x(0), y(0) {}
+  Coord(const int &X, const int &Y) : x(X), y(Y) {}
+
+  // necessary for unordered_set comparison
+  bool operator==(const Coord &other) const {
+    return (x == other.x && y == other.y);
+  }
+
+  // needed for unordered_set hashing
+  size_t operator()(const Coord &ToHash) const noexcept {
+    size_t hash = static_cast<unsigned>(ToHash.x + 137 * ToHash.y);
+    return hash;
+  }
+
+  void dump() const { std::cout << "[" << x << "," << y << "]\n"; }
+};
--- Day03.h
+++ Day03.h
@@ -1,31 +1,12 @@
 #pragma once
 
 #include "Day.h"
+#include "Coord.h"
 // #include <boost/algorithm/string.hpp>
 
 class Day03 : public Day {
 private:
   std::vector<std::string> Strings;
-  struct Coord {
-    int x;
-    int y;
-
-    Coord() : x(0), y(0) {}
-    Coord(const int &X, const int &Y) : x(X), y(Y) {}
-
-    // necessary for unordered_set comparison
-    bool operator==(const Coord &other) const {
-      return (x == other.x && y == other.y);
-    }
-
-    // needed for unordered_set hashing
-    size_t operator()(const Coord &ToHash) const noexcept {
-      size_t hash = static_cast<unsigned>(ToHash.x + 137 * ToHash.y);
-      return hash;
-    }
-
-    void dump() const { std::cout << "[" << x << "," << y << "]\n"; }
-  };
   std::unordered_set<Coord, /* hash= */ Coord> Overlappers;
 
   struct Area {
