88398b8298c0180f2e1be9dd5f69664e6ef54969
Ondrej Glasnak <glasnak.o@gmail.com>
2018-12-08 14:36:36 +0100

    Oops, uncomment actually useful structures.

--- Day04.h
+++ Day04.h
@@ -6,8 +6,8 @@
 class Day04 : public Day {
 private:
   std::vector<std::string> Strings;
-//  std::map<int, std::vector<int>> Guards;
-//  std::map<int, int> Minutes;
+  std::map<int, std::vector<int>> Guards;
+  std::map<int, int> Minutes;
 public:
   Day04() : Day(4) {}
   void solvePart1() override;
