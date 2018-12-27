207544eb7aa4a79363a6f78e53569ab932dda917
Ondrej Glasnak <glasnak.o@gmail.com>
2018-12-27 23:47:52 +0100

    Fourteenth day of Christmas: Chocolate Charts & The Optimal Hot Chocolate Recipe

--- include/Day14.h
+++ include/Day14.h
@@ -4,9 +4,13 @@
 
 class Day14 : public Day {
 private:
+  std::vector<int> Scoreboard;
+  int Elf1, Elf2;
+  void init();
+  int fillScoreboard();
 public:
-    Day14() : Day(14) {}
-    void solvePart1() override;
-    void solvePart2() override;
-    ~Day14() override = default;
+  Day14() : Day(14) {}
+  void solvePart1() override;
+  void solvePart2() override;
+  ~Day14() override = default;
 };
--- src/Day14.cpp
+++ src/Day14.cpp
@@ -2,14 +2,91 @@
 
 #include "Day14.h"
 
+/// returns number of new recipes created
+int Day14::fillScoreboard() {
+  int Elf1Recipe = Scoreboard[Elf1];
+  int Elf2Recipe = Scoreboard[Elf2];
+  int NewRecipe = Elf1Recipe + Elf2Recipe;
+  /// New recipe[s]
+  if (NewRecipe > 9) {
+    Scoreboard.push_back(1);
+    Scoreboard.push_back(NewRecipe - 10);
+  } else {
+    Scoreboard.push_back(NewRecipe);
+  }
+  size_t ScoreboardSize = Scoreboard.size();
+  /// New elf positions:
+  Elf1 = (Elf1 + 1 + Elf1Recipe) % ScoreboardSize;
+  Elf2 = (Elf2 + 1 + Elf2Recipe) % ScoreboardSize;
+  return (NewRecipe > 9) ? 2 : 1;
+}
+
+void Day14::init() {
+  Scoreboard = {3, 7};
+  Elf1 = 0;
+  Elf2 = 1;
+}
 
 void Day14::solvePart1() {
-    const Input = 990941;    // my puzzle input
-    const Input = 9;         // test input
-    std::cout << -1 << "\n";
+  const int Input = 990941;    // my puzzle input
+//  const int Input = 2018;         // test input
+  init();
+
+  for (int i = 0; i < Input * 2; ++i) {
+    fillScoreboard();
+  }
+  for (int i = Input; i < Input+10; ++i) {
+    std::cout << Scoreboard[i];
+  }
+  std::cout << "\n";
+}
+
+static std::vector<int> getDigits(int Num) {
+  std::vector<int> Digits;
+  do {
+    Digits.push_back(Num % 10);
+    Num /= 10;
+  } while (Num > 0);
+  std::reverse(Digits.begin(), Digits.end());
+  return Digits;
 }
 
 
 void Day14::solvePart2() {
-    std::cout << -1 << "\n";
+  const int Input = 990941;
+  std::vector<int> Digits = getDigits(Input);
+  init();
+  for (long i = 0; i < 100000000; ++i) {
+    int Filled = fillScoreboard();
+//    for (auto D : Scoreboard) { std::cout << D;} std::cout << "\n";
+    if (Scoreboard.back() == Digits.back()) {
+      bool Found = true;
+      for (auto DIter = Digits.rbegin(), SIter = Scoreboard.rbegin(), DEnd = Digits.rend(); DIter != DEnd; ++DIter,++SIter) {
+        if (*DIter != *SIter) {
+          Found = false;
+          break;
+        }
+      }
+      if (Found) {
+        std::cout << Scoreboard.size() - Digits.size() << "\n";
+        return;
+      }
+    }
+    // only check this in case fillScoreboard returned 2
+    if (Filled == 2 && *std::prev(Scoreboard.end(),2) == Digits.back()) {
+      bool Found = true;
+      for (auto DIter = Digits.rbegin(), SIter = std::next(Scoreboard.rbegin()), DEnd = Digits.rend(); DIter != DEnd; ++DIter,++SIter) {
+        if (*DIter != *SIter) {
+          Found = false;
+          break;
+        }
+      }
+      if (Found) {
+        std::cout << Scoreboard.size() - Digits.size() - 1 << "\n";
+        return;
+      }
+    }
+  }
+  std::cout << -1 << "\n";
 }
+
