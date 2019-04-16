52740738cab7f5af346568ec38da57687a87f078
Ondrej Glasnak <glasnak.o@gmail.com>
2019-04-16 20:58:55 +0200

    Day16: Solve part 1: Chronal Classification & deduce opcodes for part 2

--- src/Day16.cpp
+++ src/Day16.cpp
@@ -39,8 +39,37 @@ void Day16::parse(std::vector<std::string> Lines) {
   }
 }
 
-void Day16::solvePart2() {
-//  std::vector<std::string> Lines = Util::getLines("inputs/input_16.txt");
-  std::cout << -1 << "\n";
+void Day16::solvePart1() {
+  std::vector<std::string> Lines = Util::getLines("inputs/input_16.txt");
+  parse(Lines);
+  int ThreeOrMore = 0;
+  for (const auto &E : Examples) {
+    std::vector<bool> PossibleOpcodes(16, false);
+    int Expected = E.After[E.Instr[3]];
+    int Ra = E.Before[E.Instr[1]];
+    int Rb = E.Before[E.Instr[2]];
+    int Ia = E.Instr[1];
+    int Ib = E.Instr[2];
+    
+    PossibleOpcodes[0] = (Expected == (Ra + Rb));      // 6
+    PossibleOpcodes[1] = (Expected == (Ra + Ib));      // 9
+    PossibleOpcodes[2] = (Expected == (Ra * Rb));      // 14
+    PossibleOpcodes[3] = (Expected == (Ra * Ib));      // 1
+    PossibleOpcodes[4] = (Expected == (Ra & Rb));      // 2
+    PossibleOpcodes[5] = (Expected == (Ra & Ib));      // 3
+    PossibleOpcodes[6] = (Expected == (Ra | Rb));      // 12
+    PossibleOpcodes[7] = (Expected == (Ra | Ib));      // 0
+    PossibleOpcodes[8] = (Expected == (Ra));           // 5
+    PossibleOpcodes[9] = (Expected == (Ia));           // 8
+    PossibleOpcodes[10] = (Expected == (Ia > Rb));     // 4
+    PossibleOpcodes[11] = (Expected == (Ra > Ib));     // 15
+    PossibleOpcodes[12] = (Expected == (Ra > Rb));     // 13
+    PossibleOpcodes[13] = (Expected == (Ia == Rb));    // 7
+    PossibleOpcodes[14] = (Expected == (Ra == Ib));    // 11
+    PossibleOpcodes[15] = (Expected == (Ra == Rb));    // 10
+    auto Count = std::count(PossibleOpcodes.begin(), PossibleOpcodes.end(), true);
+    ThreeOrMore += (Count >= 3);
+  }
+  std::cout << /*"Over 3: " <<*/ ThreeOrMore << "\n";
 }
 
