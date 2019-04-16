c065f8a82ea032079d37cddb54cb09b2953e3048
Ondrej Glasnak <glasnak.o@gmail.com>
2019-04-16 20:58:19 +0200

    Day16: dump and parse methods added

--- include/Day16.h
+++ include/Day16.h
@@ -1,7 +1,29 @@
 #pragma once
 
+#include "Day.h"
+
 class Day16 : public Day {
 private:
+  struct Execution {
+    std::vector<int> Before; // Regs 0..3 before execution
+    std::vector<int> After;  // Regs 0..3 after execution
+    std::vector<int> Instr;  // Opc, A, B, C
+    void dump() {
+      std::cout << "==================\n";
+      std::cout << "Before: ";
+      for (auto B : Before) {std::cout << B << ", ";}
+      std::cout << "\n";
+      for (auto I : Instr) {std::cout << I << " ";}
+      std::cout << "\nAfter: ";
+      for (auto A : After) {std::cout << A << ", ";}
+      std::cout << "\n";
+      std::cout << "==================\n";
+    }
+  };
+  
+  std::vector<Execution> Examples;
+  void parse(std::vector<std::string> Lines);
+  
 public:
   Day16() : Day(16) {}
   void solvePart1() override;
--- src/Day16.cpp
+++ src/Day16.cpp
@@ -2,10 +2,41 @@
 
 #include "Day16.h"
 
-void Day16::solvePart1() {
-//  std::vector<std::string> Lines = Util::getLines("inputs/input_16.txt");
-  std::cout << -1 << "\n";
-
+/// parse this:
+// Before: [3, 2, 1, 1]
+// 9 2 1 2
+// After:  [3, 2, 2, 1]
+void Day16::parse(std::vector<std::string> Lines) {
+  auto LinesSize = Lines.size();
+  for (unsigned i = 0; i < LinesSize; ++i) {
+    Execution exec;
+    if (Lines[i][0] != 'B')
+      continue;
+    exec.Before = {0, 0, 0, 0};
+    sscanf(Lines[i].c_str(),
+           "Before: [%d, %d, %d, %d]",
+           &exec.Before[0],
+           &exec.Before[1],
+           &exec.Before[2],
+           &exec.Before[3]);
+    ++i;
+    exec.Instr = {0, 0, 0, 0};
+    sscanf(Lines[i].c_str(),
+           "%d %d %d %d",
+           &exec.Instr[0],
+           &exec.Instr[1],
+           &exec.Instr[2],
+           &exec.Instr[3]);
+    ++i;
+    exec.After = {0, 0, 0, 0};
+    sscanf(Lines[i].c_str(),
+           "After: [%d, %d, %d, %d]",
+           &exec.After[0],
+           &exec.After[1],
+           &exec.After[2],
+           &exec.After[3]);
+    Examples.emplace_back(exec);
+  }
 }
 
 void Day16::solvePart2() {
