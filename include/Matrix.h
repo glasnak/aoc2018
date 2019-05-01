3a33d21eba16303feeef6da006628f61620a0972
Ondrej Glasnak <glasnak.o@gmail.com>
2019-05-01 14:48:14 +0200

    update Matrix with useful methods for Day18 such as count and getNeighbours

--- include/Matrix.h
+++ include/Matrix.h
@@ -20,6 +20,7 @@ template <typename T> struct Matrix {
 
   /// insert an item. Return the item that was on the position before.
   T insert(T Item, Coord Pos) {
+    assert(isInRange(Pos));
     T TmpItem = M[Pos.x][Pos.y];
     M[Pos.x][Pos.y] = Item;
     return TmpItem;
@@ -28,18 +29,44 @@ template <typename T> struct Matrix {
   T insert(T Item, unsigned x, unsigned y) { return insert(Item, Coord(x, y)); }
 
   /// check if cell position fits the matrix
-  bool isInRange(Coord Cell) {
-    return Cell.x >= 0 && Cell.x < Columns && Cell.y >= 0 && Cell.y < Rows;
+  bool isInRange(Coord Cell) const {
+    return Cell.x >= 0 && Cell.x < (int) Columns && Cell.y >= 0 && Cell.y < (int) Rows;
   }
 
   /// get the underlying structure
-  std::vector<std::vector<T>> get() { return M; }
+  std::vector<std::vector<T>> get() const { return M; }
 
   /// return the underlying value on position \Cell.
-  T getValue(Coord Cell) { return M[Cell.x][Cell.y]; }
+  T getValue(Coord Cell) const { return M[Cell.x][Cell.y]; }
+
+  std::vector<T> getNeighboursOf(Coord Pos) const {
+    assert(isInRange(Pos));
+    std::vector<T> Nbors;
+    std::vector<Direction> Dirs = {NORTH, WEST, EAST, SOUTH};
+    for (const auto Dir : Dirs) {
+      Coord NewPos = Coord(Pos.x + Move[Dir].first, Pos.y + Move[Dir].second);
+      if (isInRange(NewPos)) {
+        Nbors.emplace_back(getValue(NewPos));
+      }
+    }
+    return Nbors;
+  }
+  
+  std::vector<T> getExtendedNeighboursOf(Coord Pos) const {
+    std::vector<T> Nbors = getNeighboursOf(Pos);  // 4 Nbors;
+    for (const auto VerticalDir : std::vector<Direction>({NORTH, SOUTH})) {
+      for (const auto HorizontalDir : std::vector<Direction>({WEST, EAST})) {
+        Coord NewPos = Coord(Pos.x + Move[VerticalDir].first, Pos.y + Move[HorizontalDir].second);
+        if (isInRange(NewPos)) {
+          Nbors.emplace_back(getValue(NewPos));
+        }
+      }
+    }
+    return Nbors;
+  }
 
   // return the coord of the max element
-  Coord findMaxElement(T &MaxElem) {
+  Coord findMaxElement(T &MaxElem) const {
     MaxElem = std::numeric_limits<T>::min();
     Coord MaxElemPos;
     for (unsigned i = 0; i < Rows; ++i) {
@@ -52,6 +79,14 @@ template <typename T> struct Matrix {
       }
     }
     return MaxElemPos;
+  }
+  
+  unsigned count(T Elem) const {
+    unsigned cnt = 0;
+    for (unsigned i = 0; i < Rows; ++i) {
+      cnt += std::count(M[i].begin(), M[i].end(), Elem);
+    }
+    return cnt;
   }
 
   /// print the matrix itself for debug purposes
