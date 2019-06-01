eb1381a55dd1e1ab6eab37c323c5c4ac730f91c3
Ondrej Glasnak <glasnak.o@gmail.com>
2019-06-01 19:43:18 +0200

    Twenty-third day of Christmas: Experimental Emergency Teleportation & SMTO4NR: Satisfiability Modulo Theorem Optimization for Nanobot Ranges

--- src/Day23.cpp
+++ src/Day23.cpp
@@ -1,6 +1,8 @@
 // --- Day 23: Experimental Emergency Teleportation  ---
 
 #include "Day23.h"
+#include<z3++.h>
+
 
 Day23::PointSet Day23::parse() {
 //  std::vector<std::string> Lines = Util::getLines("inputs/input_23_test2.txt");
@@ -48,5 +50,55 @@ void Day23::solvePart1() {
 }
 
 void Day23::solvePart2() {
-  std::cout << -1 << "\n";
+  auto Points = parse();
+  z3::context Context;
+  z3::expr x = Context.int_const("x");
+  z3::expr y = Context.int_const("y");
+  z3::expr z = Context.int_const("z");
+  
+  z3::expr_vector InRanges(Context);
+
+  size_t NumPoints = Points.size();
+  for (size_t i = 0; i < NumPoints; ++i) {
+    std::string InRangeStr = "in_range_" + std::to_string(i);
+    InRanges.push_back(Context.int_const(InRangeStr.c_str()));
+  }
+  auto RangeCount = Context.int_const("sum");
+
+  z3::optimize Opt(Context);
+  
+  z3::expr One = Context.int_val(1);
+  z3::expr Zero = Context.int_val(0);
+  for (size_t i = 0; i < NumPoints; ++i) {
+    std::pair<Coord3D, int> Nanobot = Points[i];
+
+    /// Manhattan distance range constraint
+    Opt.add(
+        InRanges[i] ==
+            z3::ite(
+                ((z3::abs(x - Nanobot.first.x)
+                    + z3::abs(y - Nanobot.first.y)
+                    + z3::abs(z - Nanobot.first.z)
+                ) <= Nanobot.second),
+                One, Zero));
+  }
+  
+  /// Nanobots within range, this will be maximized.
+  Opt.add(RangeCount == z3::sum(InRanges));
+  
+  /// Second constraint - be closest to zero, this will be minimized as 
+  /// a secondary constraint.
+  auto DistFromZero = Context.int_const("dist");
+  Opt.add(DistFromZero == z3::abs(x) + z3::abs(y) + z3::abs(z));
+
+  z3::optimize::handle h1 = Opt.maximize(RangeCount);
+  z3::optimize::handle h2 = Opt.minimize(DistFromZero);
+  if (Opt.check() != z3::sat) {
+    assert(!"Unsatisfiable constraints in Z3 solver!\n");
+    exit(1);  // ?
+  }
+//  std::cout << "Satisfiable? " << Opt.check() << ".\n";
+//  std::cout << "Range:" << Opt.lower(h1) << ",  " << Opt.upper(h1) << "\n";
+//  std::cout << "DistFromZero:" << Opt.lower(h2) << ",  " << Opt.upper(h2) << "\n";
+  std::cout << Opt.lower(h2) << "\n";
 }
