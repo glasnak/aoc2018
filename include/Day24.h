b9861288c82a3ef0c89d91685802aaccde43637f
Ondrej Glasnak <glasnak.o@gmail.com>
2019-06-08 19:00:19 +0200

    Day24: Add input parsing functionality

--- include/Day24.h
+++ include/Day24.h
@@ -1,11 +1,75 @@
 #pragma once
 
 #include "Day.h"
+#include <boost/algorithm/string.hpp>
+
+
 
 
 class Day24 : public Day {
 
+  enum ArmyTeam {
+    ImmuneSystem,
+    Infection
+  };
+
+  struct Unit {
+    int ArmySize;
+    int HP;
+    std::pair<int, std::string> Attack;
+    int Initiative;
+    std::vector<std::string> Immunities, Weaknesses;
+    ArmyTeam Team;
+
+    std::vector<std::string> parseWeakImmune(const std::vector<std::string> &Elements,
+                                             const std::string &WeakOrImmune) {
+      std::vector<std::string> Result;
+      auto Element = std::find(Elements.begin(), Elements.end(), WeakOrImmune);
+      if (Element != Elements.end()) {
+        std::advance(Element, 2);
+        while (Element->back() == ',') {
+          Result.emplace_back(Element->substr(0, Element->size() - 1));
+          ++Element;
+        }
+        Result.emplace_back(*Element);
+      }
+      return Result;
+    }
+    
+    Unit(const std::string &Line, const ArmyTeam AT) : Team(AT) {
+      std::vector<std::string> Elements;
+      boost::split(Elements, Line, boost::is_any_of(" ();"));
+      ArmySize = std::stoi(Elements.at(0));
+      HP = std::stoi(Elements.at(4));
+
+      auto Element = std::find(Elements.begin(), Elements.end(), "does");
+      ++Element;
+      auto AttackStrength = std::stoi(*Element);
+      ++Element;
+      Attack = std::make_pair(AttackStrength, *Element);
+      Element = std::find(Elements.begin(), Elements.end(), "initiative");
+      ++Element;
+      Initiative = std::stoi(*Element);
+
+      Weaknesses = parseWeakImmune(Elements, "weak");
+      Weaknesses = parseWeakImmune(Elements, "immune");
+    }
+
+    int getAttack() const { return ArmySize * Attack.first; }
+
+    bool operator<(const Unit &U) const {
+      return getAttack() < U.getAttack() ? true : (getAttack() == U.getAttack()
+                                                   ? (Initiative < U.Initiative)
+                                                   : false);
+    }
+    bool operator>(const Unit &U) const { return U < *this; }
+  };
+
+  std::vector<Unit> Units;
+
+  void fight();
 public:
+  friend std::ostream &operator<<(std::ostream &OS, const Unit &U);
   Day24() : Day(24) {}
   void solvePart1() override;
   void solvePart2() override;
--- src/Day24.cpp
+++ src/Day24.cpp
@@ -2,8 +2,51 @@
 
 #include "Day24.h"
 
+std::ostream &operator<<(std::ostream &OS, const Day24::Unit &U) {
+  if (U.Team == Day24::ArmyTeam::ImmuneSystem) {
+    OS << "Immune:    ";
+  } else {
+    OS << "Infection: ";
+  }
+
+  OS << U.ArmySize << " Units at " << U.HP << " HP; Attack of " << U.Attack.first
+     << " dmg of type" << U.Attack.second << "; initiative = " << U.Initiative;
+  if (!U.Weaknesses.empty()) {
+    OS << "; weak to:";
+    for (auto &weak : U.Weaknesses) {
+      OS << " " << weak;
+    }
+  }
+  if (!U.Immunities.empty()) {
+    OS << "; immune to:";
+    for (auto &immune : U.Immunities) {
+      OS << " " << immune;
+    }
+  }
+  return OS;
+}
+
+/// Fight Units to the death.c
+void Day24::fight() {
+  
+}
+
 void Day24::solvePart1() {
   std::vector<std::string> Lines = Util::getLines("inputs/input_24_test.txt");
+//  std::vector<std::string> Lines = Util::getLines("inputs/input_24.txt");
+  ArmyTeam CurrentTeam = ArmyTeam::ImmuneSystem;
+  for (const auto &Line : Lines) {
+    if (Line.empty()) {
+      CurrentTeam = ArmyTeam::Infection;
+      continue;
+    }
+    if (Line[0] == 'I')
+      continue;
+    Units.emplace_back(Line, CurrentTeam);
+  }
+  for (const auto &U : Units) {
+    std::cout << U << "\n";
+  }
   std::cout << -1 << "\n";
 }
 
